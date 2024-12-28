f8a77a9c3182fec7b92d7153a0ed9238bb8a8ace
Курицына Анна Валерьевна <wwww.anechka@gmail.com>
2024-12-28 12:28:00 +0300

    Про именнованные аргументы

--- ConsoleOld/Program.cs
+++ ConsoleOld/Program.cs
@@ -6,7 +6,20 @@ namespace ConsoleOld
     {
         static void Main(string[] args)
         {
+
             Console.WriteLine("Hello World!");
         }
+
     }
 }
+
+
+//При смешенно именнованной передаче аргументов в метод подобное поведение допустимо 
+//Foo(a: 1, 2, d: 10);  //в метод Main 
+
+//private static void Foo(int a = 0, int b = 0, int c = 0, int d = 0)
+//{
+
+//    var result = a + b + c + d;
+
+//}
