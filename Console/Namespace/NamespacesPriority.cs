b7dd9282aa7943e200b82cefcecb88d37e60c96e
Anna Mityaeva <wwww.anechka@gmail.com>
2025-01-02 22:57:41 +0300

    Namespaces priority

---
+++ Console/Namespace/NamespacesPriority.cs
@@ -0,0 +1,23 @@
+﻿//код является рабочим, не мешает сборке проекта
+
+namespace Console.Namespace
+{
+    internal class NamespacesPriority
+    {
+    }
+
+    namespace Inner
+    {
+        internal class Foo { }
+        internal class NamespacesPriority { }   //допустимо, поскольку этот класс вложенный то имя у него другое, не такое как у первого
+
+        internal class Examples
+        {
+            Foo f1 = new();
+
+            NamespacesPriority n1 = new();  //тот, что находится в одном с данным классом пространстве имен
+            Namespace.NamespacesPriority n2 = new();    //тот, что находится сверху. для этого четко прописан адрес
+
+        }
+    }
+}
