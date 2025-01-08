36cfb7c540e1cb9d7a0b5515501ea0e943c2ed89
Anna Mityaeva <wwww.anechka@gmail.com>
2025-01-08 22:40:59 +0300

    Added classes fields

---
+++ Console/Types/ClassField.cs
@@ -0,0 +1,30 @@
+﻿namespace Console.Types;
+
+internal static class ClassField
+{
+    //Именование обычного поля
+    public static string Name { get; set; }
+
+    //Вариант именования нескольких однотипных методов
+    static readonly int legs = 8,
+                        eyes = 2;
+
+
+    /// <summary>
+    /// Особенности объявления константного значения
+    /// </summary>
+    private const int age = 18;
+    //private const int age2 = new int();   //Недпустимо. ошибка на этапе компиляции
+
+    /// <summary>
+    /// Особенности объявления readonly static
+    /// </summary>
+    public readonly static DateTime day = DateTime.Now;
+
+
+    public readonly static DateTime today;  //объявление в конструкторе класса
+    static ClassField()
+    {
+        today = DateTime.Today;
+    }
+}
