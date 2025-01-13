fa61042b1f1380057d3692e56696de4caaa60e7f
Курицына Анна Валерьевна <wwww.anechka@gmail.com>
2025-01-13 16:37:56 +0300

    Added inheritance and virtual on it

---
+++ Console/Types/Inheritance.cs
@@ -0,0 +1,27 @@
+﻿namespace Console.Types;
+
+public class Asset
+{
+    public string Name { get; set; }
+    public virtual decimal Liability => 0;
+}
+
+public class Stock : Asset
+{
+    public long SharesOwned;
+
+}
+
+public class House : Asset
+{
+    public decimal Mortgage;
+    public override decimal Liability => Mortgage;      //переопределение вертуального родительского свойства
+
+    public void Something()
+    {
+        var stock = new Stock();
+
+        var liab = stock.Liability; //вызов вертуального не переопределенного свойства, то есть вернет 0
+        var name = stock.Name;
+    }
+}
