d70cd165c025ca25f4c43990fa65abef397f32f2
Курицына Анна Валерьевна <wwww.anechka@gmail.com>
2025-01-13 16:54:12 +0300

    Added abstracts on heritage

---
+++ Console/Types/Abstract.cs
@@ -0,0 +1,37 @@
+﻿namespace Console.Types;
+
+public abstract class AssetA    //Экземпляр класса не может быть создан. только наследники
+{
+    public string Name { get; set; }
+    public virtual decimal Liability => 0;
+
+    public abstract decimal NetValue { get; }       //обязательна реализация у наследников
+}
+
+public class StockA : AssetA
+{
+    public long SharesOwned;
+
+    public override decimal NetValue => throw new NotImplementedException();    //если переопределение в данном классе реализции не требуется 
+}
+
+public class HouseA : AssetA
+{
+    public decimal Mortgage;
+    public override decimal Liability => Mortgage;      //переопределение вертуального родительского свойства
+
+    public override decimal NetValue { get; }   //переопределение абстрактного члена родительского класса
+
+    public void Something()
+    {
+        var stock = new Stock();
+
+        var liab = stock.Liability; //вызов вертуального не переопределенного свойства, то есть вернет 0
+        var name = stock.Name;
+    }
+}
+
+public abstract class Warehouse : AssetA    // если наследник тоже абстрактный, то переопределения абстрактных членов не требуется
+{
+    
+}
